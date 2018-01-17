43ff266868923d2a6fddbfbd9e3bbf52a8bdfc44
Krzysztof Miemiec <krzysztof.miemiec@gmail.com>
2018-01-17 10:53:54 +0100

    Added sample notification icon to `spec/` and mentioned it in `README.md`. Added `gems/` to `.gitignore`. Changed `appicon_devices` to `appicon_icon_types`, merged `tablet` and `phone` into `launcher` type (as most Android devices are xxhdpi or xxxhdpi nowadays). Added `notification` type that can be used to generate notification icons, which replaces `notification_icons` and additional parameters. Fixed potential issue mentioned in https://github.com/KrauseFx/fastlane-plugin-appicon/issues/30 by setting default, empty `appicon_custom_sizes` parameter.

--- lib/fastlane/plugin/appicon/actions/android_appicon_action.rb
+++ lib/fastlane/plugin/appicon/actions/android_appicon_action.rb
@@ -3,36 +3,27 @@ module Fastlane
     class AndroidAppiconAction < Action
       def self.needed_icons
         {
-          phone: {
-            'ldpi' => ['36x36'],
-            'mdpi' => ['48x48'],
-            'hdpi' => ['72x72'],
-            'xhdpi' => ['96x96']
+          launcher: {
+            :ldpi => ['36x36'],
+            :mdpi => ['48x48'],
+            :hdpi => ['72x72'],
+            :xhdpi => ['96x96'],
+            :xxhdpi => ['144x144'],
+            :xxxhdpi => ['192x192']
           },
-          tablet: {
-            'xxhdpi' => ['144x144'],
-            'xxxhdpi' => ['192x192']
+          notification: {
+            :ldpi => ['18x18'],
+            :mdpi => ['24x24'],
+            :hdpi => ['36x36'],
+            :xhdpi => ['48x48'],
+            :xxhdpi => ['72x72'],
+            :xxxhdpi => ['96x96'],
           }
-        }
-      end
-      
-      def self.notification_icons(path, filename)
-        return {} if !path || !filename
-        
-        {
-          "#{path}-ldpi/#{filename}" => '36x36',
-          "#{path}-mdpi/#{filename}" => '24x24',
-          "#{path}-hdpi/#{filename}" => '36x36',
-          "#{path}-xhdpi/#{filename}" => '48x48',
-          "#{path}-xxhdpi/#{filename}" => '72x72',
-          "#{path}-xxxhdpi/#{filename}" => '96x96',
         }
       end
 
       def self.run(params)
         fname = params[:appicon_image_file]
-        notification_icon_path = params[:appicon_notification_icon_path]
-        notification_icon_filename = params[:appicon_notification_icon_filename]
         custom_sizes = params[:appicon_custom_sizes]
 
         require 'mini_magick'
@@ -42,11 +33,8 @@ module Fastlane
 
         # Convert image to png
         image.format 'png'
-        
-        # Merge notification icons into customer sizes as they are handled thes same way
-        custom_sizes = self.notification_icons(notification_icon_path, notification_icon_filename).merge(custom_sizes)
 
-        icons = Helper::AppiconHelper.get_needed_icons(params[:appicon_devices], self.needed_icons, true, custom_sizes)
+        icons = Helper::AppiconHelper.get_needed_icons(params[:appicon_icon_types], self.needed_icons, true, custom_sizes)
         icons.each do |icon|
           width = icon['width']
           height = icon['height']
@@ -54,7 +42,7 @@ module Fastlane
           # Custom icons will have basepath and filename already defined
           if icon.has_key?('basepath') && icon.has_key?('filename')
             basepath = Pathname.new(icon['basepath'])
-            filename = icon['filename']  
+            filename = icon['filename']
           else
             basepath = Pathname.new("#{params[:appicon_path]}-#{icon['scale']}")
             filename = "#{params[:appicon_filename]}.png"
@@ -67,9 +55,9 @@ module Fastlane
 
         UI.success("Successfully stored launcher icons at '#{params[:appicon_path]}'")
       end
-      
+
       def self.get_custom_sizes(image, custom_sizes)
-        
+
       end
 
       def self.description
@@ -87,15 +75,15 @@ module Fastlane
                                description: "Path to a square image file, at least 512x512",
                                   optional: false,
                                       type: String),
-          FastlaneCore::ConfigItem.new(key: :appicon_devices,
-                                  env_name: "APPICON_DEVICES",
-                             default_value: [:phone],
+          FastlaneCore::ConfigItem.new(key: :appicon_icon_types,
+                                  env_name: "APPICON_ICON_TYPES",
+                             default_value: [:launcher],
                                description: "Array of device types to generate icons for",
                                   optional: true,
                                       type: Array),
           FastlaneCore::ConfigItem.new(key: :appicon_path,
                                   env_name: "APPICON_PATH",
-                             default_value: 'app/res/mipmap/',
+                             default_value: 'app/res/mipmap',
                                description: "Path to res subfolder",
                                   optional: true,
                                       type: String),
@@ -105,20 +93,9 @@ module Fastlane
                                description: "The output filename of each image",
                                   optional: true,
                                       type: String),
-          FastlaneCore::ConfigItem.new(key: :appicon_notification_icon_path,
-                                  env_name: "APPICON_NOTIFICATION_ICON_PATH",
-                             default_value: 'app/res/drawable/',
-                               description: "Path to res subfolder",
-                                  optional: true,
-                                      type: String),
-          FastlaneCore::ConfigItem.new(key: :appicon_notification_icon_filename,
-                                  env_name: "APPICON_NOTIFICATION_ICON_FILENAME",
-                             default_value: 'ic_stat_onesignal_default',
-                               description: "File name for notification icons",
-                                  optional: true,
-                                      type: String),
           FastlaneCore::ConfigItem.new(key: :appicon_custom_sizes,
                                description: "Hash of custom sizes - {'path/icon.png' => '256x256'}",
+                             default_value: {},
                                   optional: true,
                                       type: Hash)
         ]
